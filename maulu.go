df717708729f9c34948085a5189c0dcc7eb458d3
Tulir293 <tulir293@maunium.net>
2015-12-16 22:13:39 +0200

    Backend mostly done

--- maulu.go
+++ maulu.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 	"html/template"
@@ -12,9 +13,8 @@ import (
 	"strings"
 )
 
-// GetIP gets the IP that sent the given request.
-func GetIP(r *http.Request, trustHeaders bool) string {
-	if trustHeaders {
+func getIP(r *http.Request) string {
+	if config.TrustHeaders {
 		return r.Header.Get("X-Forwarded-For")
 	}
 	return r.RemoteAddr
@@ -25,8 +25,19 @@ func query(w http.ResponseWriter, r *http.Request) {
 	reqURL := r.Form.Get("url")
 	action := r.URL.Path[len("/query/"):]
 	if action == "unshorten" {
-		log.Debugf("%[1]s requested unshortening of %[2]s", GetIP(r, config.TrustHeaders), reqURL)
-		longURL, err := queryURL(reqURL[len(config.URL):])
+		if !strings.HasPrefix(reqURL, "https://mau.lu/") {
+			log.Warnf("%[1]s attempted to unshorten an invalid URL.", getIP(r))
+			writeError(w, "The URL you entered is not a mau\\Lu short URL.")
+			return
+		}
+		shortID := reqURL[len(config.URL):]
+		if len(shortID) > 20 {
+			log.Warnf("%[1]s attempted to unshorten an impossibly long short URL", getIP(r))
+			writeError(w, "The URL you entered is too long.")
+			return
+		}
+		log.Debugf("%[1]s requested unshortening of %[2]s", getIP(r), reqURL)
+		longURL, err := queryURL(shortID)
 		if err != nil {
 			log.Warnf("Failed to find long url from short url id %[2]s: %[1]s", err, reqURL)
 			writeError(w, "The short url id %[1]s doesn't exist!", reqURL)
@@ -36,12 +47,12 @@ func query(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusFound)
 	} else if action == "google" || action == "shorten" {
 		if strings.HasPrefix(reqURL, "https://mau.lu") {
-			log.Warnf("%[1]s attempted to shorten the mau\\Lu url %[2]s", GetIP(r, config.TrustHeaders), reqURL)
+			log.Warnf("%[1]s attempted to shorten the mau\\Lu url %[2]s", getIP(r), reqURL)
 			w.Header().Add("Location", config.URL+"?url="+url.QueryEscape(reqURL))
 			w.WriteHeader(http.StatusFound)
 			return
 		} else if !strings.HasPrefix(reqURL, "https://") && !strings.HasPrefix(reqURL, "http://") {
-			log.Warnf("%[1]s attempted to shorten an URL with an unidentified protocol", GetIP(r, config.TrustHeaders))
+			log.Warnf("%[1]s attempted to shorten an URL with an unidentified protocol", getIP(r))
 			writeError(w, "Protocol couldn't be identified.")
 			return
 		}
@@ -49,15 +60,15 @@ func query(w http.ResponseWriter, r *http.Request) {
 			reqURL = "http://lmgtfy.com/?q=" + url.QueryEscape(reqURL)
 		}
 		if len(reqURL) > 255 {
-			log.Warnf("%[1]s attempted to shorten a very long URL", GetIP(r, config.TrustHeaders))
+			log.Warnf("%[1]s attempted to shorten a very long URL", getIP(r))
 			writeError(w, "The URL you entered is too long.")
 			return
 		}
-		log.Debugf("%[1]s requested shortening of %[2]s", GetIP(r, config.TrustHeaders), reqURL)
+		log.Debugf("%[1]s requested shortening of %[2]s", getIP(r), reqURL)
 		w.Header().Add("Location", config.URL+"?url="+url.QueryEscape(config.URL+insert(reqURL)))
 		w.WriteHeader(http.StatusFound)
 	} else {
-		log.Warnf("%[1]s attempted to use an unidentified action: %[2]s", GetIP(r, config.TrustHeaders), action)
+		log.Warnf("%[1]s attempted to use an unidentified action: %[2]s", getIP(r), action)
 		writeError(w, "Invalid action \"%[1]s\"", action)
 	}
 }
@@ -76,16 +87,15 @@ func get(w http.ResponseWriter, r *http.Request) {
 			index.Execute(w, struct{ URL, Error interface{} }{queryURL, ""})
 		}
 	} else if path == "favicon.ico" {
-		log.Debugf("%[1]s requested the favicon", GetIP(r, config.TrustHeaders))
 		w.Write(favicon)
 	} else {
 		// Path not recognized. Check if it's a redirect key
-		log.Debugf("%[1]s requested long url of of %[2]s", GetIP(r, config.TrustHeaders), path)
+		log.Debugf("%[1]s requested long url of of %[2]s", getIP(r), path)
 		url, err := queryURL(path)
 		if err != nil {
 			// No short url. Redirect to the index
 			log.Warnf("Failed to find redirect from short url %[2]s: %[1]s", err, path)
-			writeError(w, "Error 404: https://mau.lu/%[1]s not found", path)
+			writeError(w, "404: https://mau.lu/%[1]s not found", path)
 			return
 		}
 		// Short url identified. Redirect to long url
@@ -96,10 +106,13 @@ func get(w http.ResponseWriter, r *http.Request) {
 
 var favicon []byte
 
+var debug = flag.Bool("d", false, "Enable to print debug messages to stdout")
+
 func main() {
+	flag.Parse()
 	// Configure the logger
-	log.PrintDebug = true
-	log.Fileformat = func(s string, i int) string { return "maulu.log" }
+	log.PrintDebug = *debug
+	log.Fileformat = func(date string, i int) string { return fmt.Sprintf("logs/%[1]s-%02[2]d.log", date, i) }
 
 	// Initialize the logger
 	log.Init()
